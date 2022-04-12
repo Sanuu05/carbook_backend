3ca1de5461ae360eca9266b9b34e1301e497c850
Shantanu Saha <sansaha0505@gmail.com>
2022-04-13 00:34:07 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT| 1798
+const port = process.env.PORT || 1798
 const bodyParser = require('body-parser')
 const route = require('./routes')
 const user = require('./user')
